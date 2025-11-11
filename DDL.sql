e5bf86a6e0685dbff250e2d52edb6ee78d54bb62
Maria Luiza <mluizaguedessilva@gmail.com>
2025-11-11 08:42:35 -0300

    add seção, comentário e revisão

--- DDL.sql
+++ DDL.sql
@@ -52,6 +52,54 @@ insert into tra_trabalho (tra_titulo, tra_data_hora_entrega, tra_nota, tra_usr_i
   values ('Teste 1', current_timestamp, 6, 1),
          ('Teste 2', current_timestamp, null, 1);
 
-create user postgres with password 'postgres';
+create user spring with password 'pass123';
+
+-- A partir daqui incluí tabelas extras para estudo e avalizações
+-- Não esqueçam de executar o grant ao final
+
+alter table tra_trabalho add primary key (tra_id);
+
+create table sec_secao (
+  sec_id bigint generated always as identity,
+  sec_titulo varchar(100) not null,
+  sec_data_hora_criacao timestamp not null,
+  sec_conteudo varchar(300),
+  sec_tra_id bigint not null,
+  primary key (sec_id),
+  foreign key (sec_tra_id) references tra_trabalho (tra_id) on delete restrict on update cascade
+);
+
+insert into sec_secao (sec_titulo, sec_data_hora_criacao, sec_conteudo, sec_tra_id)
+  values ('Capítulo 1', '2023-08-04 10:30', 'Meu nome é Post', 2),
+         ('Capítulo 2', '2023-08-04 11:16', 'Trabalho com software', 2);
+
+create table com_comentario (
+  com_id bigint generated always as identity,
+  com_conteudo varchar(100) not null,
+  com_data_hora_criacao timestamp not null,
+  com_urgencia int not null,
+  com_tra_id bigint not null,
+  primary key (com_id),
+  foreign key (com_tra_id) references tra_trabalho (tra_id) on delete restrict on update cascade
+);
+
+insert into com_comentario (com_conteudo, com_data_hora_criacao, com_urgencia, com_tra_id)
+  values ('Falta conteúdo', '2025-10-20 11:00', 3, 2),
+         ('Novo', current_timestamp, 1, 1);
+
+create table rev_revisao (
+  rev_id bigint generated always as identity,
+  rev_feedback varchar(100) not null,
+  rev_data_hora_criacao timestamp not null,
+  rev_data_hora_correcao timestamp,
+  rev_sec_id bigint not null,
+  primary key (rev_id),
+  foreign key (rev_sec_id) references sec_secao (sec_id) on delete restrict on update cascade
+);
+
+
+insert into rev_revisao (rev_feedback, rev_data_hora_criacao, rev_data_hora_correcao, rev_sec_id)
+  values ('Somente título', '2025-10-20 11:00', '2025-10-27 23:05', 1),
+         ('E o resto?', current_timestamp, null, 2);
 
 grant update, delete, insert, select on all tables in schema public to spring;
